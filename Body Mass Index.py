526ccbf40a9fbbdf307e999fe6786eb59b09d85e
Daniel <daniel@gmail.com>
2025-04-18 13:23:31 +0100

    I fixed the sudden crashing of the application when you make the wrong input

--- Body Mass Index.py
+++ Body Mass Index.py
@@ -5,16 +5,20 @@
 
 constantValue = 703
 def gettingInputs():
-    weight = float(input('Enter your weight in pounds: \n'))
-    HeightInches = float(input('Enter your height in inches: \n'))
-    HeightInch = float(input('Enter Another height in inches: \n'))
-    result = float((weight * constantValue) / (HeightInches * HeightInch))
-    print(f'Your BMI is: {result}')
+    try:
+        weight = float(input('Enter your weight in pounds: \n'))
+        HeightInches = float(input('Enter your height in inches: \n'))
+        HeightInch = float(input('Enter Another height in inches: \n'))
+        result = float((weight * constantValue) / (HeightInches * HeightInch))
+        print(f'Your BMI is: {result}')
+    except ValueError:
+        print("Invalid Input")
 
 while True:
+    gettingInputs()
     progress = input('Do you want to continue? y/n where y is "yes" and n is "no"\n')
     if progress.lower() == "y" or progress.lower() == "yes":
-        gettingInputs()
+        continue
     else:
         break
 
